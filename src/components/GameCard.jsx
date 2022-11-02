17aa5e755d013f742c4d750e4deb98c57a11c4ae
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-01 23:59:02 -0300

    game detail

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from './GameCard.module.css';
 
 export function GameCard({ game }) {
     return (
         <li className={styles.gameCard}>
+            <Link to={'/games/' + game.id}>
             <img  
               src={game.background_image} 
               alt={game.name}
               className={styles.gameImage} />
             <div>{game.name}</div>
+            </Link>
         </li>
     )
 }
--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,9 +1,25 @@
 import styles from './GameDetails.module.css';
+import game from './game.json';
+import genre from './genres.json';
 
 export function GameDetails() {
+
     return (
-        <h1 className={styles.h1}>
-            GAME DETAILS
-        </h1>
+        <div className={styles.detailsContainer}>
+            <img
+              className={`${styles.col} ${styles.gameImage}`} 
+              src={game.background_image} 
+              alt={game.name}
+            />
+            <div className={`${styles.col} ${styles.gameDetails}`}>
+                <p><strong>Name</strong>: {game.name}</p>
+                <p><strong>Genre</strong>: {genre.results.map(nam => nam.name).join(", ")}</p>
+                <p><strong>Release date</strong>: {game.released}</p>
+                <p><strong>Platforms</strong>: {game.platforms.map(plat => plat.platform.slug).join(", ")}</p>
+                <p><strong>Requirements</strong>: {game.platforms.map(req => req.requirements.recommended).join(", ")}</p>
+                <p><strong>Description</strong>: {game.description}</p>
+                <p><strong>Rating</strong>: {game.rating}</p>
+            </div>
+        </div>
     )
 }
