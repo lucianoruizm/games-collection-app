20a6165e18957975d79303c161c8feb41685198f
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-29 11:39:22 -0300

    title style

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,8 +1,10 @@
-import styles from '../App.module.css';
+import styles from './MainTitle.module.css';
 
 export const MainTitle = () => {
   
     return (
-        <h1><a href="/" className={styles.title}>Portal Games Project</a></h1>
+        <div className={styles.titleContainer}>
+            <h1><a href="/" className={styles.title}>Portal Games Project</a></h1>
+        </div>
     )
 }
